1aa373c55f2f36c2ca14af181ee77d115a3b802e
CawanHenrique <cawancnd@gmail.com>
2025-08-18 20:25:28 -0300

    first commit

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'CSV Importer',
+        href: '/dashboard',
+    },
+];
+</script>
+
+<template>
+    <Head title="CsvImporter"/>
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <h1>Teste</h1>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+    
