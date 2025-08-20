0c85f073d54181ba03e10304def6941be4cb5616
CawanHenrique <cawancnd@gmail.com>
2025-08-20 16:12:38 -0300

    chore: :wrench: Invite of Dashboard.vue
    Invite of Dashboard.vue with SearchFilter and BreadCrumbs add.

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import ContactsTable from '@/components/DashboardContacts/ContactsTable.vue';
+import Button from '@/components/ui/button/Button.vue';
+import Input from '@/components/ui/input/Input.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'CSV Importer',
+        title: 'View Contacts',
         href: '/dashboard',
     },
 ];
@@ -14,14 +17,21 @@ const breadcrumbs: BreadcrumbItem[] = [
 const props = defineProps<{
     contacts: any;
 }>()
+
+const search = ref('');
+
+function submitSearch(){
+    router.get(route('dashboard'), {search: search.value})
+}
 </script>
 
 <template>
 
-    <Head title="CsvImporter" />
-
+    <Head title="Alberon Importer" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+                <Input type="search" @keyup.enter="submitSearch" v-model="search" class="border border-green-800" />
+                <Button type="button"  variant="outline" @click="submitSearch">Search</Button>
                  <ContactsTable :contacts="contacts" />
         </div>
     </AppLayout>
