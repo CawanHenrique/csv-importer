092b7c4b7575ce740202dbd47d8397a0adfbf176
CawanHenrique <cawancnd@gmail.com>
2025-08-20 16:13:10 -0300

    style: :lipstick: Alter name to Alberon.
    Alter name to Alberon.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { File,  Database } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
+        title: 'View',
         href: '/dashboard',
-        icon: LayoutGrid,
+        icon: Database,
     },
+    {
+        title: 'Contacts Import',
+        href: '/contacts',
+        icon: File
+    }
 ];
 
 </script>
